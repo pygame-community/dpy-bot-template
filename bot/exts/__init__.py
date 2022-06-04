4c44e39c6b4392350fa66bbf0cd1a56e8bfaa9bc
Mega-JC <65417594+Mega-JC@users.noreply.github.com>
2022-06-05 00:47:37 +0200

    Update __init__.py

--- bot/exts/__init__.py
+++ bot/exts/__init__.py
@@ -1,1 +1,5 @@
-
+"""This file allows the `/exts` directory to behave like a package module.
+By defining `setup()` and `teardown()` functions that take in a `discord.ext.commands.Bot`
+instance as an argument, the entire directory can be loaded as one large collection
+of extensions.
+"""
